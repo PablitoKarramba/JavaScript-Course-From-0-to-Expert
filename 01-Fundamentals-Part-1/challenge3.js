a342ad7b660ae27345fe0025e79b373ac288e47d
Paweł <73346905+PablitoKarramba@users.noreply.github.com>
2023-04-24 15:58:32 +0200

    Coding Challenge #3
    Scores comparison and result output to the console

--- 01-Fundamentals-Part-1/challenge3.js
+++ 01-Fundamentals-Part-1/challenge3.js
@@ -4,8 +4,8 @@ Coding Challenge #3
 
 TODO:
 - [X] Calculate the average score for each team
-- Compare scores to determine the winner
-- Print the result to the console
+- [X] Compare scores to determine the winner
+- [X] Print the result to the console
 
 NOTES:
 - Minimum score of 100 required
@@ -31,21 +31,18 @@ for (let number of dolphinsScoreTestData1) {
   sumDolphins += number;
 }
 averageDolphins.push(sumDolphins / dolphinsScoreTestData1.length);
-console.log(`Dolphins score after game 1: ${averageDolphins[0].toFixed(2)}`);
 
 sumDolphins = 0;
 for (let number of dolphinsScoreTestData2) {
   sumDolphins += number;
 }
 averageDolphins.push(sumDolphins / dolphinsScoreTestData2.length);
-console.log(`Dolphins score after game 2: ${averageDolphins[1].toFixed(2)}`);
 
 sumDolphins = 0;
 for (let number of dolphinsScoreTestData3) {
   sumDolphins += number;
 }
 averageDolphins.push(sumDolphins / dolphinsScoreTestData3.length);
-console.log(`Dolphins score after game 3: ${averageDolphins[2].toFixed(2)}`);
 
 //Koalas calculations
 let sumKoalas = 0;
@@ -53,22 +50,50 @@ for (let number of koalasScoreTestData1) {
   sumKoalas += number;
 }
 averageKoalas.push(sumKoalas / koalasScoreTestData1.length);
-console.log(`Koalas score after game 1: ${averageKoalas[0].toFixed(2)}`);
 
 sumKoalas = 0;
 for (let number of koalasScoreTestData2) {
   sumKoalas += number;
 }
 averageKoalas.push(sumKoalas / koalasScoreTestData2.length);
-console.log(`Koalas score after game 2: ${averageKoalas[1].toFixed(2)}`);
 
 sumKoalas = 0;
 for (let number of koalasScoreTestData3) {
   sumKoalas += number;
 }
 averageKoalas.push(sumKoalas / koalasScoreTestData3.length);
-console.log(`Koalas score after game 3: ${averageKoalas[2].toFixed(2)}`);
 
-//Game 1 results
-//Game 2 results
-//Game 3 results
+//Game results
+if (averageDolphins.length === averageKoalas.length) {
+  for (let i = 0; i < averageDolphins.length; i++) {
+    if ((averageDolphins[i] && averageKoalas[i]) < minimumScore) {
+      console.log(
+        `Both teams scored below minimum score (${averageDolphins[i].toFixed(
+          2
+        )}:${averageKoalas[i].toFixed(2)}). No winner determined for game #${
+          i + 1
+        }`
+      );
+    } else if (averageDolphins[i] > averageKoalas[i]) {
+      console.log(
+        `Dolphins won game #${i + 1} - ${averageDolphins[i].toFixed(
+          2
+        )}:${averageKoalas[i].toFixed(2)}`
+      );
+    } else if (averageDolphins[i] < averageKoalas[i]) {
+      console.log(
+        `Koalas won game #${i + 1} - ${averageDolphins[i].toFixed(
+          2
+        )}:${averageKoalas[i].toFixed(2)}`
+      );
+    } else {
+      console.log(
+        `Game #${i + 1} resulted in a draw - ${averageDolphins[i].toFixed(
+          2
+        )}:${averageKoalas[i].toFixed(2)}`
+      );
+    }
+  }
+} else {
+  console.log(`The amount of games played by both teams is nonequal`);
+}
