8a66dc3a9c8813599ef3677608587caab78a273c
Paweł <73346905+PablitoKarramba@users.noreply.github.com>
2023-05-16 22:09:57 +0200

    refactor: reduce duplicate code

--- 05-Project-Guess-My-Number/script.js
+++ 05-Project-Guess-My-Number/script.js
@@ -1,18 +1,55 @@
 'use strict';
 
 // Generate secret number
-let secretNumber = Math.trunc(Math.random() * 99 + 1);
+const generateSecret = (min, max) =>
+  Math.trunc(Math.random() * (max - min + 1)) + min;
+let secretNumber = generateSecret(1, 100);
 console.log(`Secret: ${secretNumber}`);
 
 // Calculate score
 let score = 20;
+let highscore = 0;
 const updateScore = (points) => {
   score = score + points;
-  document.querySelector('.score').textContent = score;
+  changeElementTextContent(`.score`, score);
 };
 
-// Highscore logic
-let highscore = 0;
+// Change text content of specified element
+const changeElementTextContent = (element, message) => {
+  document.querySelector(element).textContent = message;
+};
+
+// Change button pointer-events property: active/inactive (expected values: 'auto', 'none')
+const changeElementActivity = (element, value) => {
+  document.querySelector(element).style.pointerEvents = value;
+};
+
+const resetContentToDefaults = (isNewGame) => {
+  // Clear user input
+  document.querySelector('.guess').value = '';
+
+  // Reset info message
+  changeElementTextContent(`.message`, `Start guessing...`);
+  changeElementTextContent(`.number`, `?`);
+
+  // Change buttons activity
+  changeElementActivity('.check', 'auto');
+  changeElementActivity('.generate', 'none');
+
+  // Change style back to default
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+
+  // Generate new secret number
+  secretNumber = generateSecret(1, 100);
+  console.log(`New secret: ${secretNumber}`);
+
+  // Reset score if new game
+  if (isNewGame) {
+    score = 20;
+    changeElementTextContent(`.score`, score);
+  }
+};
 
 // Check button handler
 document.querySelector('.check').addEventListener('click', function () {
@@ -20,104 +57,53 @@ document.querySelector('.check').addEventListener('click', function () {
   // Compare value against secret number
   // Invalid input value
   if (!guess) {
-    // BUG: handle if user inserted 0
-    document.querySelector('.message').textContent = `Type in your guess`;
+    // BUG: user can't type 0 as a guess
+    changeElementTextContent(`.message`, `Type in your guess`);
   }
   // Correct answer
   else if (guess === secretNumber) {
-    document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('.message').textContent = `Correct guess!`;
+    changeElementTextContent(`.number`, secretNumber);
+    changeElementTextContent(`.message`, `Correct guess!`);
     updateScore(10);
 
     if (score > highscore) {
       highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
+      changeElementTextContent(`.highscore`, highscore);
     }
-
-    // Activate "generate new" button
-    document.querySelector('.generate').style.pointerEvents = 'auto';
-
-    // Deactivate "check" button
-    document.querySelector('.check').style.pointerEvents = 'none';
 
     // Change website style
     document.querySelector('body').style.backgroundColor = '#31cf39';
     document.querySelector('.number').style.width = '30rem';
-  }
-  // Guess too high
-  else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = `Value too high`;
-      updateScore(-1);
-    } else {
-      document.querySelector('.message').textContent = `You lost!`;
-      updateScore(-1);
 
-      // Deactivate "check" button
-      document.querySelector('.check').style.pointerEvents = 'none';
-    }
+    // Change buttons activity
+    changeElementActivity('.generate', 'auto');
+    changeElementActivity('.check', 'none');
   }
-  // Guess too low
-  else if (guess < secretNumber) {
+  // Wrong guess
+  else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = `Value too low`;
+      changeElementTextContent(
+        `.message`,
+        guess > secretNumber ? `Value too high` : `Value too low`
+      );
       updateScore(-1);
     } else {
-      document.querySelector('.message').textContent = `You lost!`;
+      changeElementTextContent(`.message`, `You lost!`);
       document.querySelector('body').style.backgroundColor = '#ac1e1e';
       updateScore(-1);
 
       // Deactivate "check" button
-      document.querySelector('.check').style.pointerEvents = 'none';
+      changeElementActivity('.check', 'none');
     }
   }
 });
 
-// Again button functionality (reset)
+// Again button functionality (Game reset)
 document.querySelector('.again').addEventListener('click', function () {
-  // Clear user input
-  document.querySelector('.guess').value = '';
-
-  // Reset info message
-  document.querySelector('.message').textContent = `Start guessing...`;
-  document.querySelector('.number').textContent = '?';
-
-  // Activate check button
-  document.querySelector('.check').style.pointerEvents = 'auto';
-
-  // Reset score
-  score = 20;
-  document.querySelector('.score').textContent = score;
-
-  // Change style back to default
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
-
-  // Generate new secret number
-  secretNumber = Math.trunc(Math.random() * 99 + 1);
-  console.log(`New secret: ${secretNumber}`);
+  resetContentToDefaults(true);
 });
 
 // Generate new number button handler (continue game)
 document.querySelector('.generate').addEventListener('click', function () {
-  // Clear user input
-  document.querySelector('.guess').value = '';
-
-  // Reset info message
-  document.querySelector('.message').textContent = `Start guessing...`;
-  document.querySelector('.number').textContent = '?';
-
-  // Activate check button
-  document.querySelector('.check').style.pointerEvents = 'auto';
-
-  // Change style back to default
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
-
-  // Generate new secret number
-  secretNumber = Math.trunc(Math.random() * 99 + 1);
-  console.log(`New secret: ${secretNumber}`);
-
-  // Deactivate the button
-  document.querySelector('.generate').style.pointerEvents = 'none';
+  resetContentToDefaults(false);
 });
