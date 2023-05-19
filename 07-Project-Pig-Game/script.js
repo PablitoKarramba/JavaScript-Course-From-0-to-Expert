6cef25539a00b9cfacffc8feb19260141f90da6e
Paweł <73346905+PablitoKarramba@users.noreply.github.com>
2023-05-19 16:53:16 +0200

    feat: Add roll dice functionality
    - Current score counter
    - Switching players

--- 07-Project-Pig-Game/script.js
+++ 07-Project-Pig-Game/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Select DOM elements
+const player0Section = document.querySelector('.player--0');
+const player1Section = document.querySelector('.player--1');
 const totalScorePlayer0 = document.querySelector('#score--0');
 const totalScorePlayer1 = document.querySelector('#score--1');
 const currentScorePlayer0 = document.querySelector('#current--0');
@@ -13,6 +15,7 @@ const btnHold = document.querySelector('.btn--hold');
 const imgDice = document.querySelector('.dice');
 
 // Set starting conditions
+let isPlayer0Active = true;
 let currentScore = 0;
 totalScorePlayer0.textContent = 0;
 totalScorePlayer1.textContent = 0;
@@ -23,7 +26,6 @@ imgDice.classList.add('hidden'); // Hide dice image at game start
 btnRollDice.addEventListener('click', function () {
   // Generate random value
   const diceRollValue = Math.floor(Math.random() * 6 + 1);
-  console.log(diceRollValue);
 
   // Show dice image
   imgDice.classList.remove('hidden');
@@ -32,7 +34,24 @@ btnRollDice.addEventListener('click', function () {
   // Check dice value
   if (diceRollValue !== 1) {
     // Add roll value to score
+    currentScore += diceRollValue;
+    if (isPlayer0Active) {
+      currentScorePlayer0.textContent = currentScore;
+    } else {
+      currentScorePlayer1.textContent = currentScore;
+    }
   } else {
     // Switch player
+    isPlayer0Active = !isPlayer0Active;
+    currentScore = 0;
+
+    // Change style
+    if (isPlayer0Active) {
+      player0Section.classList.add('player--active');
+      player1Section.classList.remove('player--active');
+    } else {
+      player1Section.classList.add('player--active');
+      player0Section.classList.remove('player--active');
+    }
   }
 });
