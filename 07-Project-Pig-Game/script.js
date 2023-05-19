cbe1ebf3528332c7606b965427d51a54b8d6cef1
Paweł <73346905+PablitoKarramba@users.noreply.github.com>
2023-05-20 01:26:04 +0200

    feat: Add "hold" button handler

--- 07-Project-Pig-Game/script.js
+++ 07-Project-Pig-Game/script.js
@@ -17,8 +17,10 @@ const imgDice = document.querySelector('.dice');
 // Set starting conditions
 let isPlayer0Active = true;
 let currentScore = 0;
-totalScorePlayer0.textContent = 0;
-totalScorePlayer1.textContent = 0;
+let player0TotalScore = 0;
+let player1TotalScore = 0;
+totalScorePlayer0.textContent = player0TotalScore;
+totalScorePlayer1.textContent = player1TotalScore;
 
 imgDice.classList.add('hidden'); // Hide dice image at game start
 
@@ -48,5 +50,46 @@ btnRollDice.addEventListener('click', function () {
     // Change style
     player0Section.classList.toggle('player--active');
     player1Section.classList.toggle('player--active');
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  // Add current score to total score
+  if (isPlayer0Active) {
+    // Player 0
+    player0TotalScore += currentScore;
+    totalScorePlayer0.textContent = player0TotalScore;
+    if (totalScorePlayer0 >= 100) {
+      // Player 0 wins
+      player0Section.classList.add('player--winner');
+      imgDice.classList.add('hidden');
+    } else {
+      // Switch player
+      isPlayer0Active = !isPlayer0Active;
+      currentScore = 0;
+      currentScorePlayer0.textContent = 0;
+
+      // Change style
+      player0Section.classList.toggle('player--active');
+      player1Section.classList.toggle('player--active');
+    }
+  } else {
+    // Player 1
+    player1TotalScore += currentScore;
+    totalScorePlayer1.textContent = player1TotalScore;
+    if (totalScorePlayer1 >= 100) {
+      // Player 1 wins
+      player1Section.classList.add('player--winner');
+      imgDice.classList.add('hidden');
+    } else {
+      // Switch player
+      isPlayer0Active = !isPlayer0Active;
+      currentScore = 0;
+      currentScorePlayer1.textContent = 0;
+
+      // Change style
+      player0Section.classList.toggle('player--active');
+      player1Section.classList.toggle('player--active');
+    }
   }
 });
