4df600f5a6c38778cc130bac87ecb30e2871b154
Paweł <73346905+PablitoKarramba@users.noreply.github.com>
2023-05-29 19:18:59 +0200

    docs: This keyword behavior test

--- 08-Scoping/script.js
+++ 08-Scoping/script.js
@@ -1,1 +1,57 @@
 'use strict';
+
+// -----===== This keyword =====-----
+// This value depending on context:
+// 1. Method
+const peter = {
+  age: 25,
+  calcAge: function () {
+    console.log(this); // Object { age: 25, calcAge: calcAge() }
+  },
+};
+peter.calcAge();
+
+// 2. Simple function call
+const calcAge = function () {
+  console.log(this); // undefined only in strict mode
+  // if not strict mode then it points to Window object
+};
+calcAge();
+
+// 3. Arrow function
+const calcAgeArrow = () => {
+  console.log(this); // Window
+};
+calcAgeArrow();
+
+// 4. Event listener
+document.querySelector('.btn').addEventListener('click', function () {
+  console.log(this); // <button class="btn">
+});
+
+const obiekt = {
+  age: 25,
+  calcAge: function () {
+    // This will be undefined, can't access age property
+    const isAdult = function () {
+      //console.log(this.age); TypeError: can't access property "age", this is undefined
+    };
+
+    // Pre ES6 solution
+    const self = this;
+    const isAdult1 = function () {
+      console.log(self.age); // 25
+    };
+
+    // ES6+ solution: Arrow function
+    const isAdult2 = () => {
+      console.log(this.age); // 25
+      // because parent scope is obiekt
+    };
+    isAdult();
+    isAdult1();
+    isAdult2();
+  },
+};
+
+obiekt.calcAge();
